48986e77203b893e71c50baf096b43ccb97b6b99
Richard Frovarp <rfrovarp@gmail.com>
2018-09-02 19:49:14 -0500

    Basically a copy from the academic side.
    Likely doesn't run, but at least it compile.s

---
+++ src/main/java/edu/ndsu/eci/capstone_exchange_sponsors/auth/ILACRealm.java
@@ -0,0 +1,54 @@
+// Copyright 2018 North Dakota State University
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package edu.ndsu.eci.capstone_exchange_sponsors.auth;
+
+import org.apache.cayenne.PersistenceState;
+import org.apache.commons.codec.binary.StringUtils;
+import org.apache.tapestry5.plastic.MethodInvocation;
+import org.apache.tapestry5.services.Environment;
+
+import edu.ndsu.eci.capstone_exchange_sponsors.persist.Proposal;
+import edu.ndsu.eci.capstone_exchange_sponsors.services.UserInfo;
+
+public class ILACRealm extends BaseILACRealm {
+
+  /** ability to edit a proposal */
+  public static final String PROPOSAL_EDIT_INSTANCE = "proposal_edit:instance";
+
+  private final UserInfo userInfo;
+
+  public ILACRealm(Environment environment, UserInfo userInfo) {
+    super(environment);
+    this.userInfo = userInfo;
+  }
+
+  @InstanceAccessMethod(PROPOSAL_EDIT_INSTANCE) 
+  public boolean isProposalEditMemeber() {
+    MethodInvocation invocation = getInvocation();
+
+    if (invocation == null) {
+      return false;
+    }
+
+    Proposal proposal = (Proposal) invocation.getParameter(0);
+
+    if (proposal.getPersistenceState() == PersistenceState.TRANSIENT) {
+      return true;
+    }
+
+    return StringUtils.equals(proposal.getUser().getFederatedId(), userInfo.getUser().getFederatedId());
+
+  }
+
+}
