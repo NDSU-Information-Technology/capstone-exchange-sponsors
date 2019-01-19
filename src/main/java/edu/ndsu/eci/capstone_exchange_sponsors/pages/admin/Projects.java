ca7b5d05e56c41d056d18ae9444089338aae87b4
TimHunter <tim.m.hunter@ndsu.edu>
2019-01-18 20:00:54 -0600

    Added ProjectStatus changing functionality to Projects page under Admin.

--- src/main/java/edu/ndsu/eci/capstone_exchange_sponsors/pages/admin/Projects.java
+++ src/main/java/edu/ndsu/eci/capstone_exchange_sponsors/pages/admin/Projects.java
@@ -17,16 +17,27 @@ import java.util.List;
 
 import org.apache.cayenne.ObjectContext;
 import org.apache.cayenne.query.SelectQuery;
+import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Zone;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Request;
+import org.apache.tapestry5.services.ajax.AjaxResponseRenderer;
 import org.apache.tapestry5.services.javascript.JavaScriptSupport;
+
+import com.googlecode.tapestry5cayenne.annotations.CommitAfter;
 
 import edu.ndsu.eci.capstone_exchange_sponsors.persist.CapstoneDomainMap;
 import edu.ndsu.eci.capstone_exchange_sponsors.persist.Project;
 import edu.ndsu.eci.capstone_exchange_sponsors.util.ProjectStatus;
 
+/**
+ * Manages Projects status.
+ *
+ */
 public class Projects {
 
+  /** Cayenne database context */
   @Inject
   private ObjectContext context;
   
@@ -34,8 +45,29 @@ public class Projects {
   @Inject
   private JavaScriptSupport javaScriptSupport;
   
+  /** Project row selection for either grid */
   @Property
   private Project row;
+  
+  /** Holds ProjectStatus value for updating a Project */
+  @Property
+  private ProjectStatus status;
+  
+  /** Zone surrounding all projects grid */
+  @Component
+  private Zone allZone;
+  
+  /** Zone surrounding pending projects grid */
+  @Component
+  private Zone pendingZone;
+  
+  /** Request for Zone notification */
+  @Inject
+  private Request request;
+  
+  /** Renderer for Zone update */
+  @Inject
+  private AjaxResponseRenderer ajaxResponseRenderer;
   
   /**
    * After render to include JS files.
@@ -44,12 +76,59 @@ public class Projects {
     javaScriptSupport.require("bootstrap/tab");
   }
   
+  /**
+   * Get list of Projects with pending ProjectStatus.
+   * @return List of Project objects.
+   */
   public List<Project> getPendingProjects() {
     return CapstoneDomainMap.getInstance().performProjectByStatus(context, ProjectStatus.PENDING);
   }
   
+  /**
+   * Get list of all Projects.
+   * @return List of Project objects.
+   */
   @SuppressWarnings("unchecked")
   public List<Project> getAllProjects() {
     return context.performQuery(new SelectQuery(Project.class));
+  }
+  
+  /**
+   * Approval eventlink for pending grid.
+   * @param row The Project being updated.
+   */
+  @CommitAfter
+  public void onApprove(Project row) {
+    row.setProjectStatus(ProjectStatus.APPROVED);
+    if (request.isXHR()) {
+      ajaxResponseRenderer.addRender(allZone);
+      ajaxResponseRenderer.addRender(pendingZone);
+    }
+  }
+  
+  /**
+   * Denial eventlink for pending grid.
+   * @param row The Project being updated.
+   */
+  @CommitAfter
+  public void onDeny(Project row) {
+    row.setProjectStatus(ProjectStatus.DECLINED);
+    if (request.isXHR()) {
+      ajaxResponseRenderer.addRender(allZone);
+      ajaxResponseRenderer.addRender(pendingZone);
+    }
+  }
+  
+  /**
+   * General ProjectStatus updating for all grid.
+   * @param row The Project being updated.
+   */
+  @CommitAfter
+  public void onSuccessFromStatusForm(Project row) {
+    row.setProjectStatus(status);
+    if (request.isXHR()) {
+      ajaxResponseRenderer.addRender(allZone);
+      ajaxResponseRenderer.addRender(pendingZone);
+    }
   }
 }
