0d95d07a31aac04f8c193aa6f01671366e3c8c12
Richard Frovarp <richard.frovarp@ndsu.edu>
2019-05-23 15:50:45 -0500

    Point to production URL for updates.

--- src/main/java/edu/ndsu/eci/capstone_exchange_sponsors/services/impl/DataSynchronizationImpl.java
+++ src/main/java/edu/ndsu/eci/capstone_exchange_sponsors/services/impl/DataSynchronizationImpl.java
@@ -1,6 +1,18 @@
+// Copyright 2019 North Dakota State University
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package edu.ndsu.eci.capstone_exchange_sponsors.services.impl;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
@@ -40,7 +52,7 @@ public class DataSynchronizationImpl implements DataSynchronization {
   /** the base path of the remote system to communicate with.
    * FIXME get from JNDI
    */
-  private final String remoteBasePath = "https://wings.it.ndsu.edu/cap/";
+  private final String remoteBasePath = "https://apps.ndsu.edu/international-capstone-exchange";
 
   /** http client that is thread safe */
   private final CloseableHttpClient httpclient;
